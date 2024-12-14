56e4dedb3c908c01dbf8c4fb8c547f332f1defb6
defford <defford@gmail.com>
2024-12-14 13:18:30 -0330

    Finished Home page

--- src/screens/Events.jsx
+++ src/screens/Events.jsx
@@ -4,19 +4,31 @@ import eventsData from '../data/eventsData.json'
 
 const Events = () => {
   return (
-    <div className="eventsList">
+    <div className="eventsSection">
+      <div className="eventsHeader">
+        <h1>Events</h1>
+        <h3>Where and when art is being passed on in Newfoundland</h3>
+        <p>Join us at craft fairs, markets, and art festivals where Elizabeth showcases her work and connects with the community. We host interactive events, including online and in-person painting classes, workshops, and artist meetups. Whether you're an aspiring artist, a professional, or simply love local art, there’s a place for you.</p>
+      </div>
+
+      <div className="eventsListSection">
+        <div className="eventsList">
+          
+          {eventsData.map((event) => (
+              <Event
+              key={event.id}
+              id={event.id}
+              date={event.date}
+              title={event.title}
+              description={event.description}
+              location={event.location}
+              time={event.time}
+              />
+              ))}
+        </div>
+
+      </div>
       
-      {eventsData.map((event) => (
-          <Event
-          key={event.id}
-          id={event.id}
-          date={event.date}
-          title={event.title}
-          description={event.description}
-          location={event.location}
-          time={event.time}
-          />
-          ))}
     </div>
   )
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -130,14 +130,7 @@ const Home = () => {
         </div>
       </div>
       <div className="eventsSection">
-        <div className="eventsHeader">
-          <h1>Events</h1>
-          <h3>Where and when art is being passed on in Newfoundland</h3>
-          <p>Join us at craft fairs, markets, and art festivals where Elizabeth showcases her work and connects with the community. We host interactive events, including online and in-person painting classes, workshops, and artist meetups. Whether you're an aspiring artist, a professional, or simply love local art, there’s a place for you.</p>
-        </div>
-        <div className="eventsListSection">
           <Events />
-        </div>
       </div>
 
       <div className="newsletterSection">
